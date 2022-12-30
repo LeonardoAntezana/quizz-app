85d005ba33f4763fa1fad4991bf12167f66f2331
¨Leonardo <leonardoantezana59@gmail.com>
2022-12-30 14:02:11 -0300

    install react-router and incorpored

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <BrowserRouter>
       <div className="App">
-
       </div>
+    </BrowserRouter>
   )
 }
 
